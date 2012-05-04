80dbfbbee5df2f1439b133f1405120bfb5715ac4
Yuri Albuquerque <yuridenommus@gmail.com>
2012-05-04 17:27:30 -0400

    Basic Rails structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrowdMind::Application.config.secret_token = 'd53a8fb5b9c8b3a72f5eaf81bd529e41c0cc4f0551862f6329b12f7ae99c8207454566c226394b5a5cd4fbfeb5f2329d800f14b8403ec93b9803999f850552a4'
